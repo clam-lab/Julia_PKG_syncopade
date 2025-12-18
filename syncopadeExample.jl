a5d42dc5264f618a3467026e55c0795bdf373fbc
DrRayCobb <raycobb.mczamlie@gmail.com>
2025-12-18 21:15:40 +0900

    comment and update

--- syncopadeExample.jl
+++ syncopadeExample.jl
@@ -1,3 +1,8 @@
+# １．サーバが実行完了時に返事が届いた時に呼び出される関数
+# jobId: ジョブID
+# ok: trueなら成功 falseなら失敗
+# value: okがtrueなら結果値、falseならエラー情報
+# 何か実行したいならここを書き換える
 function result_handler(jobId, ok, value)
     println("RESULT:")
     println(" jobId = ", jobId)
@@ -5,15 +10,18 @@ function result_handler(jobId, ok, value)
     println(" value = ", value)
 end
 
+# ２．クライアントコードを読み込む
 include("syncopadeClient.jl")
 
-# 2. クライアント側の結果受信用サーバ
+# ３. クライアント側の結果受信用サーバを起動
+# ポート番号は自分で指定する
 syncopade_result_server_once(9001, result_handler)
 
+# ４．計算要求を送信するクライアントの設定を作成
 client = SyncopadeClient(
     "192.168.2.99",  # server ip
     8099,         # server port（環境に合わせて）
-    "127.0.0.1",  # self ip 自分のIPアドレス
+    "192.168.2.99",  # self ip 自分のIPアドレス
     9001,         # self port　なんでもいい 数字を変えればパラレルで複数作れる
     "testScript", # .jl file name（拡張子なし）
     "testScript4syncopade", # module name
@@ -21,5 +29,6 @@ client = SyncopadeClient(
     ["hogehoge"] # args
 )
 
+# ５．計算要求を送信
 jobId = syncopade_calc_request(client)
 println("submitted jobId = ", jobId)
