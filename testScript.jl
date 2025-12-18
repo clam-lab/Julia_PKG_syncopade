e3062a79356cd544022b3086bfd8234193edc35b
DrRayCobb <raycobb.mczamlie@gmail.com>
2025-12-18 21:03:16 +0900

    update:test.jl to recieve the arg

--- testScript.jl
+++ testScript.jl
@@ -1,9 +1,9 @@
-
 module testScript4syncopade
 
-    function test_syncopade()
-        # Just test
+    function test_syncopade(arg::String)
         println("Running syncopade tests...")
+        println("Received arg = ", arg)
+        return "ARG=" * arg
     end
 
 end
