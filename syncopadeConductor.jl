d4b4edc4dc1b5e92562cf80a55977813fec63914
mitsuru endo <81297452+DrRayCobb@users.noreply.github.com>
2025-12-22 15:07:22 +0900

    list functionized

--- syncopadeConductor.jl
+++ syncopadeConductor.jl
@@ -3,8 +3,12 @@ struct NODES
     port::Int
 end
 
-# 利用可能な可能性のあるノードのリスト
-nodes = NODES[] 
-push!(nodes, NODES("192.168.100.26", 8026)) # C-3PX - Phyduck
-push!(nodes, NODES("192.168.100.30", 8030)) # 
+# 利用可能な可能性のあるノードのリストを返す関数
+function  geneAvailableNodeList()
+    nodes = NODES[] 
+    push!(nodes, NODES("192.168.100.26", 8026)) # C-3PX - Phyduck
+    push!(nodes, NODES("192.168.100.30", 8030)) # 
+    
+    return nodes        
+end
 
