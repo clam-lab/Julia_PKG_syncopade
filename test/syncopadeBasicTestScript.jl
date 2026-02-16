b236db09552fb7244133753a896306d9ba96f685
mitsuru endo <81297452+DrRayCobb@users.noreply.github.com>
2026-02-16 14:09:32 +0900

    Update syncopadeBasicTestScript.jl

--- test/syncopadeBasicTestScript.jl
+++ test/syncopadeBasicTestScript.jl
@@ -1,1 +1,18 @@
 # Basic test script for end-to-end Syncopade system testing.
+
+module syncopadeBasicTestScript
+
+"""
+    mock_objective_product(x, p)
+
+Mock numerical objective:
+- `x`: design-variable vector
+- `p`: parameter vector
+
+Returns the product of all elements in `x` and `p`.
+"""
+function mock_objective_product(x::AbstractVector, p::AbstractVector)
+    return prod(x) * prod(p)
+end
+
+end
